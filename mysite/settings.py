7685d13850dfa6381bacd9fa3f23ae022266b7d1
krzysztofurban <k.urban@g.pl>
2017-06-28 18:14:51 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # EMAIL CONFIG
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'djangotrenerurban@gmail.com'
-EMAIL_HOST_PASSWORD = 'ronia12345'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
